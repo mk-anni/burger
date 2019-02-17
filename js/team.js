47adf961f17b16a1451f5d6257a5fa0db943a455
Anna Makarova <makarovaann@ro.ru>
2019-02-17 23:23:12 +0300

    js3

--- js/team.js
+++ js/team.js
@@ -1,5 +1,24 @@
 const items = document.querySelectorAll(".team_item");
 
 for (const item of items) {
-  console.log(items);
+item.addEventListener('click', e => {
+  const curItem = e.currentTarget;
+  const content = curItem.querySelector('.hidden_team');
+  const textBlock = content.firstElementChild;
+  const regHeight = textBlock.clientHeight;
+  if (curItem.classList.contains('active')) {
+    //открыт
+    curItem.classList.remove('active');
+    content.style.height = '0';
+  } else {
+    curItem.classList.add('active');
+    content.style.height = `${regHeight}px`;
+  }
+  //curItem.classList.add('active')
+  //console.log(regHeight);
+  //content.style.height = `${regHeight}px`;
+
+  //content.style.height = '100%';
+  //content.style.overflow = 'visible';
+});
 }
