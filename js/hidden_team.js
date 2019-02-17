41912373942e93a6e79116541da4e4d99c1f29e3
Anna Makarova <makarovaann@ro.ru>
2019-02-17 21:29:22 +0300

    js2

---
+++ js/hidden_team.js
@@ -0,0 +1,12 @@
+//const items = document.querySelectorAll('.team_item');
+//for (item of items) {
+  //hidden.addEventListener ('click', function() {
+    //items.style.height = '100%';
+    //items.style.overflow = 'visible';
+  //})
+//}
+
+//hidden.addEventListener ('click', function () {
+  //hidden.style.height = '100%';
+  //hidden.style.overflow = 'visible';
+//})
---
+++ js/team.js
@@ -0,0 +1,5 @@
+const items = document.querySelectorAll(".team_item");
+
+for (const item of items) {
+  console.log(items);
+}
